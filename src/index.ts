821ebec5abca69234c5d551c0b3138e3ce33c542
Carl Winkler <carl@carlossus.com>
2015-09-16 13:57:04 +0800

    Add unit tests for ceil and floor functions

--- src/index.ts
+++ src/index.ts
@@ -2,98 +2,97 @@ var ko;
 var requireExists = typeof window === 'undefined'
     ? typeof require === 'function'
     : typeof window['require'] === 'function';
-    
+
 if (typeof ko === 'undefined' && !requireExists)
     throw new Error("Unable to load knockout");
-
 
 ko = ko || typeof window === 'undefined' ? require('knockout') : window['require']('knockout');
 
 export class Calendar {
-    
+
     events: KnockoutObservableArray<any> = ko.observableArray([]);
     startDay = ko.observable(0);
-    endDay = ko.computed(() => this.startDay()+6);
-    
+    endDay = ko.computed(() => this.startDay() + 6);
+
     addEvent(date: Date, object: any): void {
-        
+
     }
-    
-    getEventsForDate(date: Date|number, dateDay?: number): any[] {
+
+    getEventsForDate(date: Date | number, dateDay?: number): any[] {
         return null;
     }
-    
+
     getEvents(): any[] {
         return null;
     }
-    
+
     getEventsByWeek(): WeekEvents {
         return null;
     }
-    
+
     weeksInDateRange(start: Date, end: Date) {
         var weeks = 0;
         var iterator = start;
-        
+
         while (iterator <= end) {
             weeks++;
             iterator.setDate(iterator.getDate() + 7);
         }
-        
+
         return weeks;
     }
-    
+
     isSameDate(left: Date, right: Date): boolean {
         var sameYear = left.getFullYear() === right.getFullYear();
         var sameMonth = left.getMonth() === right.getMonth();
         var sameDay = left.getDate() === right.getDate();
-        
+
         return sameYear && sameMonth && sameDay;
-    } 
-     
+    }
+
     isSameWeek(left: Date, right: Date): boolean {
         var sameYear = left.getFullYear() === right.getFullYear();
-        var sameMonth = left.getMonth() === right.getMonth();        
+        var sameMonth = left.getMonth() === right.getMonth();
         if (!sameYear || !sameMonth) return false;
-        
-        
+
+
     }
-    
+
     floorToDay(date: Date) {
         return new Date(date.getFullYear(), date.getMonth(), date.getDate());
     }
-    
+
     ceilingToDay(date: Date) {
         var upDate = date;
         upDate.setDate(upDate.getDate() + 1);
         return new Date(upDate.getFullYear(), upDate.getMonth(), upDate.getDate());
     }
-    
+
     floorToWeekStart(date: Date) {
         var currentDay = date.getDay();
         var toDay = this.startDay();
         var downDate = date;
-                
-        if (currentDay > toDay) 
-            downDate.setDate(downDate.getDate() - (currentDay - toDay)); 
-                
-        if (currentDay < toDay) 
+
+        if (currentDay > toDay)
+            downDate.setDate(downDate.getDate() - (currentDay - toDay));
+
+        if (currentDay < toDay)
             downDate.setDate(downDate.getDate() - (currentDay + (7 - toDay)));
-        
+
         return this.floorToDay(downDate);
     }
-    
+
     ceilingToWeekEnd(date: Date) {
         var currentDay = date.getDay();
         var toDay = this.endDay();
         var upDate = date;
-        
+
         if (currentDay > toDay)
-            upDate.setDate(upDate.getDate() + (7-currentDay+toDay));
-            
+            upDate.setDate(upDate.getDate() + (7 - currentDay + toDay));
+
         if (currentDay < toDay)
             upDate.setDate(upDate.getDate() + (toDay - currentDay));
-            
+
         return this.ceilingToDay(upDate);
     }
 }
---
+++ test/index.ts
@@ -0,0 +1,67 @@
+import chai = require('chai');
+import KoCal = require('../src/index');
+var expect = chai.expect;
+
+var cal = new KoCal.Calendar();
+
+describe('equivalence tests', () => {
+
+    it('will correctly find two dates equivalent', () => {
+        var first = new Date(2015, 1, 1, 1, 1, 1);
+        var second = new Date(2015, 1, 1, 23, 59, 59);
+        expect(cal.isSameDate(first, second)).to.be.true;
+    });
+
+    it('will correctly find two dates to not be equivalent', () => {
+        var first = new Date(2015, 1, 1, 1, 1, 1);
+        var second = new Date(2015, 1, 2, 0, 0, 1);
+        expect(cal.isSameDate(first, second)).to.be.false;
+    });
+
+});
+
+describe('floor and ceiling tests', () => {
+
+    it('will correctly floor a given date', () => {
+        var now = new Date();
+        var floor = cal.floorToDay(now);
+
+        expect(cal.isSameDate(now, floor)).to.be.true;
+        expect(floor.getHours()).to.equal(0);
+        expect(floor.getMinutes()).to.equal(0);
+        expect(floor.getSeconds()).to.equal(0);
+    });
+
+    it('will correctly ceiling a given date', () => {
+        var now = new Date();
+
+        var tomorrow = now;
+        tomorrow.setDate(tomorrow.getDate() + 1);
+
+        var ceil = cal.ceilingToDay(now);
+        expect(cal.isSameDate(tomorrow, ceil)).to.be.true;
+        expect(ceil.getHours()).to.equal(0);
+        expect(ceil.getMinutes()).to.equal(0);
+        expect(ceil.getSeconds()).to.equal(0);
+    });
+
+    it('will correctly floor a date to start of week', () => {
+        var now = new Date(2015, 8, 16); // Wed 16 Sep 2015
+        var floor = cal.floorToWeekStart(now);
+        var expected = new Date(2015, 8, 13); // Sun 13 Sep 2015
+        expect(cal.isSameDate(floor, expected)).to.be.true;
+        expect(floor.getHours()).to.equal(0);
+        expect(floor.getMinutes()).to.equal(0);
+    });
+
+    it('will correctly ceiling a date to end of week', () => {
+        var now = new Date(2015, 8, 16); // Wed 16 Sep 2015 00:00:00
+        var ceil = cal.ceilingToWeekEnd(now);
+        var expected = new Date(2015, 8, 20); // Sun 20 Sep 2015 00:00:00
+        expect(cal.isSameDate(ceil, expected)).to.be.true;
+        expect(ceil.getHours()).to.equal(0);
+        expect(ceil.getMinutes()).to.equal(0);
+    });
+    
+
+});
