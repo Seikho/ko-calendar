a4f878beb477ca4af472b343e9f8e5d245d03843
Carl Winkler <carl@carlossus.com>
2015-09-17 15:33:31 +0800

    Change Event type to accommodate userObjects as Dates

--- src/typings/ko-calendar/ko-calendar.d.ts
+++ src/typings/ko-calendar/ko-calendar.d.ts
@@ -4,40 +4,40 @@ declare module "ko-calendar" {
 
     class Calendar {
         constructor(parser?: Parser);
-        
+
         parser: Parser;
-        
+
         eventsDate: KnockoutObservable<Date>;
-        
+
         events: KnockoutObservableArray<any>;
-        
+
         startDay: KnockoutObservable<number>;
-        
+
         endDay: KnockoutComputed<number>;
 
         addEvent(userObject: any): void;
-        
+
         eventsByDay: KnockoutComputed<Array<DayEvent>>;
-        
+
         eventsByWeek: KnockoutComputed<Array<WeekEvent>>;
-        
+
         eventsForDate: KnockoutComputed<DayEvent>;
-        
+
         weeksInDateRange(start: Date, end: Date): number;
-        
+
         isSameDate(left: Date, right: Date): boolean;
-        
+
         isSameWeek(left: Date, right: Date): boolean;
-        
+
         floorToDay(date: Date): Date;
-        
+
         ceilToDay(date: Date): Date;
-        
+
         floorToWeekStart(date: Date): Date;
-        
+
         ceilToWeekEnd(date: Date): Date;
-        
-        getDateRange(): DateRange;        
+
+        getDateRange(): DateRange;
     }
 
     interface Parser {
@@ -53,10 +53,10 @@ declare module "ko-calendar" {
         date: Date;
         events: Event[];
     }
-    
+
     interface Event {
-        __date: Date;
-        [key: string]: any;
+        date: Date;
+        value: any;
     }
 
     interface DateRange {
