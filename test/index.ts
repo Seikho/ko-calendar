fdc66679e9e1a72331818960a649f2f82118c0ee
Carl Winkler <carl@carlossus.com>
2015-10-20 11:32:33 +0800

    Remove equiv and mutate tests

--- test/index.ts
+++ test/index.ts
@@ -9,58 +9,6 @@ var cal: Types.Calendar = new Calendar();
 
 var baseDate = new Date(2015, 0, 1); // Thu, 1 Jan 2015
 
-describe('equivalence tests', () => {
-
-    it('will find two dates equivalent', () => {
-        var first = new Date(2015, 1, 1, 1, 1, 1);
-        var second = new Date(2015, 1, 1, 23, 59, 59);
-        expect(cal.isSameDate(first, second)).to.be.true;
-    });
-
-    it('will find two dates to not be equivalent', () => {
-        var first = new Date(2015, 1, 1, 1, 1, 1);
-        var second = new Date(2015, 1, 2, 0, 0, 1);
-        expect(cal.isSameDate(first, second)).to.be.false;
-    });
-
-    it('will find two days are in the same week', () => {
-        var first = new Date(2015, 8, 13, 0, 0, 0); // Sun 13 Sep 2015 00:00:00
-        var second = new Date(2015, 8, 19, 23, 59, 59); // Sun 19 Sep 2015 23:59:59
-        expect(cal.isSameWeek(first, second)).to.be.true;
-    });
-
-    it('will find two are are not in the same week', () => {
-        var first = new Date(2015, 8, 13, 0, 0, 0); // Sun 13 Sep 2015
-        var second = new Date(2015, 8, 20, 0, 0, 0); // Sun 20 Sep 2015 00:00:00
-        expect(cal.isSameWeek(first, second)).to.be.false;
-    });
-
-    it('will find that a date is in range of a DateRange', () => {
-        var range = {
-            start: getDate(),
-            end: getDate(3)
-        };
-        expect(cal.isInRange(getDate(), range)).to.be.true;
-        expect(cal.isInRange(getDate(1), range)).to.be.true;
-        expect(cal.isInRange(getDate(2), range)).to.be.true;
-        expect(cal.isInRange(getDate(3), range)).to.be.true;
-    });
-
-    it('will find that a date is not in range of a DateRange', () => {
-        var range = {
-            start: getDate(),
-            end: getDate(3)
-        };
-        range.end.setSeconds(range.end.getSeconds() - 1);
-
-        var justBefore = new Date(getDate().setSeconds(getDate().getSeconds() - 1));
-        var justAfter = new Date(getDate(3).setSeconds(getDate(3).getSeconds() + 1));
-        expect(cal.isInRange(justBefore, range)).to.be.false;
-        expect(cal.isInRange(justAfter, range)).to.be.false;
-    });
-
-});
-
 describe('ordered days of week tests', () => {
     it('will calculate the ordered days of the week with no data provided', () => {
         var weekDays = cal.weekDays();
@@ -118,53 +66,6 @@ describe('start/end day value tests', () => {
         expect(cal.endDay()).to.equal(6);
     });
 })
-
-describe('floor and ceiling tests', () => {
-
-    it('will floor a given date', () => {
-        var now = new Date();
-        var floor = cal.floorToDay(now);
-        var expected = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0, 0, 0);
-
-        expect(floor.toUTCString()).to.equal(expected.toUTCString());
-    });
-
-    it('will ceil a given date', () => {
-        var now = new Date();
-        var ceil = cal.ceilToDay(now);
-        var expected = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 23, 59, 59, 999);
-
-        expect(ceil.toUTCString()).to.equal(expected.toUTCString());
-    });
-
-    it('will ceil a date that has 00:00:00 time to 23:59:59.999 the same day', () => {
-        cal.startDay(0);
-        var testDate = new Date(2015, 1, 1, 0, 0, 0);
-        var ceil = cal.ceilToDay(testDate);
-        var expected = new Date(testDate.getFullYear(), testDate.getMonth(), testDate.getDate(), 23, 59, 59, 999);
-
-        expect(ceil.toUTCString()).to.equal(expected.toUTCString());
-    });
-
-    it('will floor a date to start of week', () => {
-        var now = new Date(2015, 8, 16); // Wed 16 Sep 2015
-        var floor = cal.floorToWeekStart(now);
-        var expected = new Date(2015, 8, 13, 0, 0, 0); // Sun 13 Sep 2015
-        
-        expect(floor.toUTCString()).to.equal(expected.toUTCString());
-    });
-
-    it('will ceil a date to end of week', () => {
-        var now = new Date(2015, 8, 16); // Wed 16 Sep 2015 00:00:00
-        var ceil = cal.ceilToWeekEnd(now);
-        var expected = new Date(2015, 8, 19); // Sun 19 Sep 2015 23:59:59.000
-        expect(cal.isSameDate(ceil, expected)).to.be.true;
-        expect(ceil.getHours()).to.equal(23);
-        expect(ceil.getMinutes()).to.equal(59);
-        expect(ceil.getSeconds()).to.equal(59);
-        expect(ceil.getMilliseconds()).to.equal(999);
-    });
-});
 
 describe('get events tests', () => {
     addRange(0); // Thu 1 Jan
